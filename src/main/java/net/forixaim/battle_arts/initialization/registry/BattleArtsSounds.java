ca90d35eae0aac44ff3579f55c6e2f1ef343a2b9
Matt <thebest20067@gmail.com>
2026-06-06 09:48:00 -0700

    sound tweak

--- src/main/java/net/forixaim/battle_arts/initialization/registry/BattleArtsSounds.java
+++ src/main/java/net/forixaim/battle_arts/initialization/registry/BattleArtsSounds.java
@@ -1,6 +1,8 @@
 package net.forixaim.battle_arts.initialization.registry;
 
 import net.forixaim.battle_arts.BattleArts;
+import net.forixaim.battle_arts.generated.LangKeys;
+import net.forixaim.battle_arts.generated.SoundKeys;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
@@ -12,14 +14,14 @@ public final class BattleArtsSounds
 {
     public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, BattleArts.MOD_ID);
 
-    public static final DeferredHolder<SoundEvent, SoundEvent> JUMP = registerSound("entity.combat.jump");
-    public static final DeferredHolder<SoundEvent, SoundEvent> UNSHEATHE = registerSound("entity.combat.unsheathe");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SHEATHE = registerSound("entity.combat.sheathe");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPIKE = registerSound("entity.combat.spike");
-    public static final DeferredHolder<SoundEvent, SoundEvent> SPECIAL_MOVE = registerSound("entity.combat.special_move");
-    public static final DeferredHolder<SoundEvent, SoundEvent> HEAVY_SLASH = registerSound("entity.combat.heavy_slash");
-    public static final DeferredHolder<SoundEvent, SoundEvent> CRITICAL_HIT = registerSound("entity.combat.critical_hit");
-    public static final DeferredHolder<SoundEvent, SoundEvent> CRITICAL_HIT_DQ8 = registerSound("entity.combat.critical_hit_8");
+    public static final DeferredHolder<SoundEvent, SoundEvent> JUMP = registerSound(SoundKeys.ENTITY_COMBAT_JUMP);
+    public static final DeferredHolder<SoundEvent, SoundEvent> UNSHEATHE = registerSound(SoundKeys.ENTITY_COMBAT_UNSHEATHE);
+    public static final DeferredHolder<SoundEvent, SoundEvent> SHEATHE = registerSound(SoundKeys.ENTITY_COMBAT_SHEATHE);
+    public static final DeferredHolder<SoundEvent, SoundEvent> SPIKE = registerSound(SoundKeys.ENTITY_COMBAT_SPIKE);
+    public static final DeferredHolder<SoundEvent, SoundEvent> SPECIAL_MOVE = registerSound(SoundKeys.ENTITY_COMBAT_SPECIAL_MOVE);
+    public static final DeferredHolder<SoundEvent, SoundEvent> HEAVY_SLASH = registerSound(SoundKeys.ENTITY_COMBAT_HEAVY_SLASH);
+    public static final DeferredHolder<SoundEvent, SoundEvent> CRITICAL_HIT = registerSound(SoundKeys.ENTITY_COMBAT_CRITICAL_HIT);
+    public static final DeferredHolder<SoundEvent, SoundEvent> CRITICAL_HIT_DQ8 = registerSound(SoundKeys.ENTITY_COMBAT_CRITICAL_HIT_8);
 
 
 
